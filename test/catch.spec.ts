027f87cd04006cbb127ce891fa55323c5ed5e99d
Meirion Hughes <crakinshot@yahoo.com>
2019-10-05 10:22:56 +0100

    test: add catch tests

--- test/catch.spec.ts
+++ test/catch.spec.ts
@@ -2,9 +2,44 @@ import { expect } from "chai";
 import { Rowan, Catch } from "../src";
 
 describe("Catch", () => {
-  it("", async () => {
-    let rowan = new Rowan();
+  it("catches errors", async () => {
+    let wasCalled = false;
+    let ctx = {};
+    let result = undefined;
+    let expected = "foo bar";
 
+    let _catch = new Catch(async (err, ctx) => { result = err });
+
+    _catch.use(async (ctx) => {
+      wasCalled = true;
+      throw expected;
+    });
+
+    await _catch.process(ctx, () => Promise.resolve());
+
+    expect(wasCalled).to.be.true;
+    expect(result).to.be.eql(expected);
+  });
+
+  it("catches errors and can rethrow", async () => {
+    let wasCaught = false;
+    let ctx = {};
+    let result = undefined;
+    let expected = "foo bar";
+
+    let _catch = new Catch(async (err, ctx) => { wasCaught = true; throw err });
+
+    _catch.use(async (ctx) => {
+      throw expected;
+    });
+
+    try {
+      await _catch.process(ctx, () => Promise.resolve());
+    } catch (err) {
+      result = err;
+    }
     
+    expect(wasCaught).to.be.true;
+    expect(result).to.be.eql(expected);
   });
 });
